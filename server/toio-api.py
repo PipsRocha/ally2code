31279ae9e3905adb160f8bdbca7d1e937c0297ea
Filipa Rocha <pipa_sr@hotmail.com>
2024-05-01 17:51:15 +0100

    svelte integration

---
+++ server/toio-api.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# ************************************************************
+#
+#     toio-api.py
+#     toio API commands for tangible programming
+#
+# ************************************************************
+
+import asyncio
+from toio import *
+from flask import Flask
+app = Flask(__name__)
+
+"""
+   directions for toio movements
+    x: left motor speed
+    y: right motor speed
+"""
+moves = {
+    "forward": {
+        "x": 10,
+        "y": 10
+    },
+    "backward": {
+        "x": -10,
+        "y": -10
+    },
+    "left": {
+        "x": 2,
+        "y": 8
+    },
+    "right": {
+        "x": 8,
+        "y": 2
+    }
+}
+
+
+""" toio cube """
+global cube
+cube : ToioCoreCube | None = None
+
+
+"""
+    connecting to one toio cube
+"""
+@app.get("/connect")
+async def connect():
+    global cube
+    device_list = await BLEScanner.scan(1)
+    if len(device_list) == 0:
+        return "No devices found", 400
+    cube = ToioCoreCube(device_list[0].interface)
+    await cube.connect()
+    return "Connected"
+
+
+"""
+    disconnecting toio cube
+"""
+@app.get("/disconnect")
+async def disconnect():
+    global cube
+    if cube is None:
+        return "Cube not connected", 400
+    
+    await cube.disconnect()
+    cube = None
+    return "Disconnected"
+
+
+"""
+    moving toio in four directions
+    <direction>: forward, backward, left, right
+    x: left motor speed
+    y: right motor speed
+"""    
+@app.get("/move/<direction>")
+async def move(direction):
+    global cube
+    if cube is None:
+        return "Cube not connected", 400
+    if direction not in moves:
+        return "Invalid direction", 400
+    
+    move = moves[direction]
+    await cube.api.motor.motor_control(move["x"], move["y"])
+    await asyncio.sleep(2)
+    await cube.api.motor.motor_control(0, 0)
+    return "Moved"
+
+
+"""
+    command for toio to dance, movement and 8-bit sounds
+"""
+@app.get("/dance")
+async def dance():
+    global cube
+    if cube is None:
+        return "Cube not connected", 400
+
+    await cube.api.motor.motor_control(10, 0)
+    await cube.api.sound.play_midi(1,[2,1,2])
+    await asyncio.sleep(2)
+    await cube.api.motor.motor_control(0, 0)
+    return "Danced"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,145 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+	import * as Select from '$lib/components/ui/select';
+	import { Separator } from '$lib/components/ui/separator';
+	import type { Mode, TopCode } from '$lib/types';
+	import { onMount } from 'svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+
+	const modes: { label: string; value: Mode }[] = [
+		{ label: 'No Awareness', value: 'no-awareness' },
+		{ label: 'Private', value: 'private' },
+		{ label: 'Shared', value: 'shared' }
+	];
+
+	let mode: { label: string; value: Mode } = modes[0];
+	let robotIP: string = '127.0.0.1:5000';
+
+	let topCodesModule: any;
+	let topCodes: number[] = [];
+
+	$: processTopCodes(topCodes);
+
+	const notEqualsCheck = (a: number[], b: number[]) => {
+		return JSON.stringify(a) !== JSON.stringify(b);
+	};
+
+	async function processTopCodes(topcodes: number[]) {
+		console.log('HERE');
+		// enters pov
+		for (let i = 0; i < topcodes.length; i++) {
+			//if(mode.value == 'shared')
+			//play earcon to speakers
+			//if play
+			if (topcodes[i] == 681) {
+				//playRobotSound(681);
+				console.log('do something');
+				await new Promise((r) => setTimeout(r, 2000));
+			}
+		}
+	}
+
+	onMount(async () => {
+		const { TopCodes } = await import('$lib/topcodes');
+		topCodesModule = TopCodes;
+		topCodesModule.setVideoFrameCallback('video-canvas', function (jsonString: string) {
+			var json = JSON.parse(jsonString);
+			const newTopCodes = json.topcodes.map((c: any) => c.code);
+			if (notEqualsCheck(topCodes, newTopCodes)) {
+				topCodes = newTopCodes;
+			}
+			//topCodes = json.topcodes;
+		});
+		topCodesModule.startStopVideoScan('video-canvas');
+	});
+
+
+	function playRobotSound(arg1: number, angle = 0) {
+		if ( arg1 === 681) {
+			// earcon sound
+
+		} else if( arg1 === 115) {
+			//say something
+
+		} else if( arg1 === 589) {
+			// dance sound
+
+		} else if( arg1 === 31) {
+			switch(angle){
+				case 1:
+					// sound for forward
+					break;
+				case 2:
+					// sound for backward
+					break;
+				case 3:
+					// sound for left
+					break;
+				case 4:
+					// sound for right
+					break;
+			}
+		}
+	}
+
+	function privateActions(topcodes: TopCode[]) {
+
+	}
+
+	function sharedActions() {
+
+	}
+
+	function noAwarenessActions() {
+		
+	}
+
+
+	function checkOutputs() {
+        navigator.mediaDevices.getUserMedia({ audio: true });
+        navigator.mediaDevices.enumerateDevices().then(function (devices) {
+            devices.forEach(function (device) {
+                console.log(device.kind + ': ' + device.label + ' id = ' + device.deviceId);
+            });
+        });
+    }
+
+	function playtoSpeakers(){
+
+	}
+
+	function playtoHeadphones(){
+
+	}
+</script>
+
+<div class="flex h-full flex-col">
+	<div class="container flex h-16 flex-row items-center justify-between py-4">
+		<h1 class="text-xl font-bold">Inclusive Robots</h1>
+		<div class="flex flex-row items-center gap-x-2">
+			<Button on:click={()=>fetch(`http://${robotIP}/connect`)}>
+				Connect to toio
+			</Button>
+			<Select.Root bind:selected={mode}>
+				<Select.Trigger class="w-48">
+					<Select.Value placeholder="Select a mode" />
+				</Select.Trigger>
+				<Select.Content>
+					{#each modes as mode}
+						<Select.Item value={mode.value} label={mode.label}>{mode.label}</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+			<Input class="w-48" bind:value={robotIP} placeholder="192.168.1.1" />
+			<Button variant="outline" on:click={checkOutputs}>Devices</Button>
+		</div>
+	</div>
+	<Separator />
+	<div class="flex flex-1 flex-row overflow-y-auto">
+		<canvas id="video-canvas" width="600" height="600"></canvas>
+		<div class="ml-auto h-[800px] w-96">
+			<pre>{JSON.stringify(topCodes, null, 2)}</pre>
+		</div>
+	</div>
+</div>
